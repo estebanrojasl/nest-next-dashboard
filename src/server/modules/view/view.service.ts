06ac86585d587fde75e8d24c6927a3a17540b1e0
estebanrojasl <esteban7590@hotmail.com>
2023-03-09 14:37:52 -0500

    .

--- src/server/modules/view/view.service.ts
+++ src/server/modules/view/view.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import next from 'next';
-// import NextServer from 'next/dist/next-server/server/next-server';
 
 @Injectable()
 export class ViewService implements OnModuleInit {
